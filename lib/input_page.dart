da68816c6c7ceda0a7322ffb6b798db88cf5bf0a
Artwot <jsarabiar98@gmail.com>
2022-12-29 11:27:02 -0600

    Refactor code in input_page.dart

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import 'card_content.dart';
+import 'reusable_card.dart';
+
 const double bottomContainerHeight = 80.0;
 const Color reusableCardColor = Color(0xFF1E1F32);
 const Color bottomContainerColor = Color(0xFF93F3DE);
@@ -76,54 +79,6 @@ class _InputPageState extends State<InputPage> {
           // )
         ],
       ),
-    );
-  }
-}
-
-class CardContent extends StatelessWidget {
-  const CardContent({
-    required this.icon,
-    required this.label,
-  });
-
-  final IconData icon;
-  final String label;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          icon,
-          size: 80.0,
-          color: Color(0xFF8E8E99),
-        ),
-        SizedBox(height: 15.0),
-        Text(
-          label.toUpperCase(),
-          style: TextStyle(color: Color(0xFF8E8E99)),
-        )
-      ],
-    );
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-  ReusableCard({required this.color, required this.cardChild});
-
-  final Color color;
-  final Widget cardChild;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15.0),
-        color: color,
-      ),
-      child: cardChild,
     );
   }
 }
